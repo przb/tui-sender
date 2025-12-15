c5fafff3f450c3e80edcad700d282ae30e96351e
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2025-12-14 22:37:16 -0600

    do reader and writer for same socket

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::net::ToSocketAddrs;
+use std::{
+    io::{Read, Write},
+    net::ToSocketAddrs,
+    sync::mpsc::{self, Receiver, Sender},
+};
 
-use color_eyre::eyre::{Context, Result, bail};
+use color_eyre::eyre::{Context, Result};
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 struct Foo {
@@ -13,13 +17,16 @@ struct FooResponse {
     message: String,
 }
 
-fn server<A>(addr: A) -> Result<()>
+fn server<A>(addr: A, ready: Sender<()>) -> Result<()>
 where
     A: ToSocketAddrs,
 {
     let s = std::net::TcpListener::bind(addr).context("failed to bind to socket")?;
+    let mut buf = vec![0; 1_000];
 
-    let (stream, addrs) = s.accept().context(format!(
+    ready.send(()).unwrap();
+
+    let (mut stream, addrs) = s.accept().context(format!(
         "failed to accept connection (at {})",
         s.local_addr()
             .map(|ad| ad.to_string())
@@ -29,7 +36,18 @@ where
     // TODO log this instead
     println!("accepted connection at {}", addrs);
 
-    let f: Foo = serde_json::from_reader(stream).context("failed to read from stream")?;
+    let num_read = stream
+        .read(&mut buf)
+        .context("failed to read from stream")?;
+
+    let f: Foo = serde_json::from_slice(&buf.get(0..num_read).unwrap()).with_context(|| {
+        format!(
+            "failed to deserialize data:\n{}",
+            String::from_utf8_lossy(&buf)
+        )
+    })?;
+
+    buf.clear();
 
     let message = format!(
         "Hello {}, you are {} years old? That's ancient!",
@@ -38,25 +56,47 @@ where
 
     let response = FooResponse { message };
 
-    println!("response: {response:?}");
+    let res_bytes = serde_json::to_vec(&response).expect("failed to serialize response");
+    let () = stream
+        .write_all(&res_bytes)
+        .context("failed to write to stream")?;
 
     Ok(())
 }
 
-fn client<A>(addr: A) -> Result<()>
+fn client<A>(addr: A, read: Receiver<()>) -> Result<()>
 where
     A: ToSocketAddrs,
 {
-    let stream =
+    read.recv().unwrap();
+
+    let mut stream =
         std::net::TcpStream::connect(addr).context("failed to connect to remote server")?;
+    let mut buf = vec![0; 1_000];
 
     let request = Foo {
         name: "Oreo".into(),
         age: 1,
     };
 
-    serde_json::to_writer(stream, &request).context("client failed to write to tcp stream")?;
+    let req_bytes = serde_json::to_vec(&request).context("failed to serialize request")?;
+    stream
+        .write_all(&req_bytes)
+        .context("client failed to write to tcp stream")?;
 
+    let num_read = stream
+        .read(&mut buf)
+        .context("failed to read from stream")?;
+
+    let response: FooResponse = serde_json::from_slice(buf.get(0..num_read).unwrap())
+        .with_context(|| {
+            format!(
+                "failed to deserialize response:\n{}",
+                String::from_utf8_lossy(&buf)
+            )
+        })?;
+
+    println!("got res: {response:?}");
     Ok(())
 }
 
@@ -65,13 +105,14 @@ fn main() -> Result<()> {
 
     println!("Hello, world!");
 
+    let (ready_tx, ready_rx) = mpsc::channel();
     let client_addr = String::from("127.0.0.1:8888");
     let server_addr = client_addr.clone();
 
-    let client_jh = std::thread::spawn(|| client(client_addr));
-    let server_jh = std::thread::spawn(|| server(server_addr));
+    let client_jh = std::thread::spawn(|| client(client_addr, ready_rx));
+    let server_jh = std::thread::spawn(|| server(server_addr, ready_tx));
 
-    let () = server_jh.join().unwrap().context("server failed")?;
+    let () = server_jh.join().unwrap().context("server failure")?;
     let () = client_jh.join().unwrap().context("client failed")?;
 
     Ok(())
