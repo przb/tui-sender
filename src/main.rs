35b25694112c977cfbb9b72b4922875040089ff8
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2025-12-14 22:40:42 -0600

    modularize the client and server

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,124 @@
-use std::{
-    io::{Read, Write},
-    net::ToSocketAddrs,
-    sync::mpsc::{self, Receiver, Sender},
-};
+use std::sync::mpsc;
 
 use color_eyre::eyre::{Context, Result};
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
-struct Foo {
-    name: String,
-    age: u8,
+mod msgs {
+
+    #[derive(Debug, Clone, Hash, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
+    pub struct Foo {
+        pub name: String,
+        pub age: u8,
+    }
+
+    #[derive(Debug, Clone, Hash, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
+    pub struct FooResponse {
+        pub message: String,
+    }
 }
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
-struct FooResponse {
-    message: String,
-}
-
-fn server<A>(addr: A, ready: Sender<()>) -> Result<()>
-where
-    A: ToSocketAddrs,
-{
-    let s = std::net::TcpListener::bind(addr).context("failed to bind to socket")?;
-    let mut buf = vec![0; 1_000];
-
-    ready.send(()).unwrap();
-
-    let (mut stream, addrs) = s.accept().context(format!(
-        "failed to accept connection (at {})",
-        s.local_addr()
-            .map(|ad| ad.to_string())
-            .unwrap_or("[unknown address]".into())
-    ))?;
-
-    // TODO log this instead
-    println!("accepted connection at {}", addrs);
-
-    let num_read = stream
-        .read(&mut buf)
-        .context("failed to read from stream")?;
-
-    let f: Foo = serde_json::from_slice(&buf.get(0..num_read).unwrap()).with_context(|| {
-        format!(
-            "failed to deserialize data:\n{}",
-            String::from_utf8_lossy(&buf)
-        )
-    })?;
-
-    buf.clear();
-
-    let message = format!(
-        "Hello {}, you are {} years old? That's ancient!",
-        f.name, f.age
-    );
-
-    let response = FooResponse { message };
-
-    let res_bytes = serde_json::to_vec(&response).expect("failed to serialize response");
-    let () = stream
-        .write_all(&res_bytes)
-        .context("failed to write to stream")?;
-
-    Ok(())
-}
-
-fn client<A>(addr: A, read: Receiver<()>) -> Result<()>
-where
-    A: ToSocketAddrs,
-{
-    read.recv().unwrap();
-
-    let mut stream =
-        std::net::TcpStream::connect(addr).context("failed to connect to remote server")?;
-    let mut buf = vec![0; 1_000];
-
-    let request = Foo {
-        name: "Oreo".into(),
-        age: 1,
+mod server {
+    use color_eyre::{Result, eyre::Context};
+    use std::{
+        io::{Read, Write},
+        net::ToSocketAddrs,
+        sync::mpsc::Sender,
     };
 
-    let req_bytes = serde_json::to_vec(&request).context("failed to serialize request")?;
-    stream
-        .write_all(&req_bytes)
-        .context("client failed to write to tcp stream")?;
+    use crate::msgs::{Foo, FooResponse};
 
-    let num_read = stream
-        .read(&mut buf)
-        .context("failed to read from stream")?;
+    pub fn server<A>(addr: A, ready: Sender<()>) -> Result<()>
+    where
+        A: ToSocketAddrs,
+    {
+        let s = std::net::TcpListener::bind(addr).context("failed to bind to socket")?;
+        let mut buf = vec![0; 1_000];
 
-    let response: FooResponse = serde_json::from_slice(buf.get(0..num_read).unwrap())
-        .with_context(|| {
+        ready.send(()).unwrap();
+
+        let (mut stream, addrs) = s.accept().context(format!(
+            "failed to accept connection (at {})",
+            s.local_addr()
+                .map(|ad| ad.to_string())
+                .unwrap_or("[unknown address]".into())
+        ))?;
+
+        // TODO log this instead
+        println!("accepted connection at {}", addrs);
+
+        let num_read = stream
+            .read(&mut buf)
+            .context("failed to read from stream")?;
+
+        let f: Foo = serde_json::from_slice(&buf.get(0..num_read).unwrap()).with_context(|| {
             format!(
-                "failed to deserialize response:\n{}",
+                "failed to deserialize data:\n{}",
                 String::from_utf8_lossy(&buf)
             )
         })?;
 
-    println!("got res: {response:?}");
-    Ok(())
+        buf.clear();
+
+        let message = format!(
+            "Hello {}, you are {} years old? That's ancient!",
+            f.name, f.age
+        );
+
+        let response = FooResponse { message };
+
+        let res_bytes = serde_json::to_vec(&response).expect("failed to serialize response");
+        let () = stream
+            .write_all(&res_bytes)
+            .context("failed to write to stream")?;
+
+        Ok(())
+    }
+}
+
+mod client {
+    use color_eyre::{Result, eyre::Context};
+    use std::{
+        io::{Read, Write},
+        net::ToSocketAddrs,
+        sync::mpsc::Receiver,
+    };
+
+    use crate::msgs::{Foo, FooResponse};
+
+    pub fn client<A>(addr: A, read: Receiver<()>) -> Result<()>
+    where
+        A: ToSocketAddrs,
+    {
+        read.recv().unwrap();
+
+        let mut stream =
+            std::net::TcpStream::connect(addr).context("failed to connect to remote server")?;
+        let mut buf = vec![0; 1_000];
+
+        let request = Foo {
+            name: "Oreo".into(),
+            age: 1,
+        };
+
+        let req_bytes = serde_json::to_vec(&request).context("failed to serialize request")?;
+        stream
+            .write_all(&req_bytes)
+            .context("client failed to write to tcp stream")?;
+
+        let num_read = stream
+            .read(&mut buf)
+            .context("failed to read from stream")?;
+
+        let response: FooResponse = serde_json::from_slice(buf.get(0..num_read).unwrap())
+            .with_context(|| {
+                format!(
+                    "failed to deserialize response:\n{}",
+                    String::from_utf8_lossy(&buf)
+                )
+            })?;
+
+        println!("got res: {response:?}");
+        Ok(())
+    }
 }
 
 fn main() -> Result<()> {
@@ -109,8 +130,8 @@ fn main() -> Result<()> {
     let client_addr = String::from("127.0.0.1:8888");
     let server_addr = client_addr.clone();
 
-    let client_jh = std::thread::spawn(|| client(client_addr, ready_rx));
-    let server_jh = std::thread::spawn(|| server(server_addr, ready_tx));
+    let client_jh = std::thread::spawn(|| client::client(client_addr, ready_rx));
+    let server_jh = std::thread::spawn(|| server::server(server_addr, ready_tx));
 
     let () = server_jh.join().unwrap().context("server failure")?;
     let () = client_jh.join().unwrap().context("client failed")?;
